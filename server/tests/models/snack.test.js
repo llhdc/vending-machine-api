cdb27d3462d7584f7ea151aa492f62e53f6339c1
Leonard <leonard.horne@gmail.com>
2017-08-15 11:18:28 -0400

    Create model test.

---
+++ server/tests/models/snack.test.js
@@ -0,0 +1,17 @@
+const db = require("../../models/index");
+const Snack = db.snack;
+
+describe('Snack', () => {
+
+  afterEach(() => {
+    return Snack.destroy({ where: {} })
+  });
+
+
+  it ('can be persisted', () => {
+    return Snack.create({ description: 'Reese\'s', price: 75, quantity: 10})
+    .then(item => {
+      expect(item.id).toBeTruthy();
+    })
+  });
+});
