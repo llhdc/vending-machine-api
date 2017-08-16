f7fda6da00ebcaf8fa81f389607990096a04ba04
Leonard <leonard.horne@gmail.com>
2017-08-16 08:42:31 -0400

    Change model variable from var to const, add foreign key.

--- server/models/snack.js
+++ server/models/snack.js
@@ -1,6 +1,7 @@
 'use strict';
+
 module.exports = function(sequelize, DataTypes) {
-  var snack = sequelize.define('snack', {
+  const snack = sequelize.define('snack', {
     description: DataTypes.TEXT,
     price: DataTypes.INTEGER,
     quantity: DataTypes.INTEGER
@@ -8,6 +9,7 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        snack.hasMany(customer, { foreignKey: 'snackId' });
       }
     }
   });
