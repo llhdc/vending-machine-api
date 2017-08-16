18012988651b1e3656c84441f3831d85b872df6a
Leonard <leonard.horne@gmail.com>
2017-08-16 09:30:57 -0400

    Add post functionality.

--- server/routes/purchase.js
+++ server/routes/purchase.js
@@ -2,10 +2,23 @@ const express = require('express');
 const router = express.Router();
 const db = require("../models")
 const Snack = db.snack;
+const Customer = db.customer;
 
 
 router.post('/api/customer/items/:itemId/purchases', (req, res) => {
-  res.send('blah');
+  Snack.findById(parseInt(req.params.id))
+  .then(snack => {
+    if ((Customer.cashOnHand > snack.price) && (snack.quantity >= 1)) {
+      snack.quantity -= 1;
+      snack.save()
+      .then(snack => {
+        res.send(`Thank you. Enjoy your ${snack}. Your change is ${parseInt(Customer.cashOnHand) - parseInt(snack.price)}`);
+      })
+    } 
+    else {
+      res.json("Sorry, you can't have that snack")
+    }
+  })
 });
 
 module.exports = router
