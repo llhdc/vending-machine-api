b9a5b43631fa99297ff85498c87815f11a7c81c1
Leonard <leonard.horne@gmail.com>
2017-08-16 08:43:05 -0400

    Modify data persistence test.

--- server/tests/models/snack.test.js
+++ server/tests/models/snack.test.js
@@ -1,17 +1,18 @@
 const db = require("../../models/index");
 const Snack = db.snack;
 
-describe('Snack', () => {
-
+describe("Snack", () => {
   afterEach(() => {
-    return Snack.destroy({ where: {} })
+    return Snack.destroy({ where: {} });
   });
 
-
-  it ('can be persisted', () => {
-    return Snack.create({ description: 'Reese\'s', price: 75, quantity: 10})
-    .then(item => {
+  it("can be persisted", () => {
+    return Snack.create({
+      description: "Reese's",
+      price: 75,
+      quantity: 10
+    }).then(item => {
       expect(item.id).toBeTruthy();
-    })
+    });
   });
 });
