4d2a351f997e45321c771254f1ff52122db344b8
Leonard <leonard.horne@gmail.com>
2017-08-16 10:06:27 -0400

    Test connection with purchase router.

--- server/routes/purchase.js
+++ server/routes/purchase.js
@@ -6,19 +6,32 @@ const Customer = db.customer;
 
 
 router.post('/api/customer/items/:itemId/purchases', (req, res) => {
-  Snack.findById(parseInt(req.params.id))
-  .then(snack => {
-    if ((Customer.cashOnHand > snack.price) && (snack.quantity >= 1)) {
-      snack.quantity -= 1;
-      snack.save()
-      .then(snack => {
-        res.send(`Thank you. Enjoy your ${snack}. Your change is ${parseInt(Customer.cashOnHand) - parseInt(snack.price)}`);
-      })
-    } 
-    else {
-      res.json("Sorry, you can't have that snack")
-    }
-  })
+  // Snack.findById(parseInt(req.params.itemId))
+  // .then(snack => {
+  //   console.log(snack);
+  //   if ((Customer.cashOnHand > snack.price) && (snack.quantity >= 1)) {
+  //     snack.quantity -= 1;
+  //     snack.save()
+  //     .then(snack => {
+  //       res.json({
+  //         status: "success",
+  //         data: snack
+  //       })
+  //       // res.send(`Thank you. Enjoy your ${snack}. Your change is ${parseInt(Customer.cashOnHand) - parseInt(snack.price)}`);
+  //     })
+  //   }
+  //   else {
+  //     res.json({
+  //       status: "failure",
+  //       data: {
+  //         "money_given": `${Customer.cashOnHand}`,
+  //         "money_required": `${snack.price}`
+  //       }
+  //     })
+  //   }
+  // })
+
+  res.send('blah')
 });
 
 module.exports = router
--- server/tests/routes/purchase.test.js
+++ server/tests/routes/purchase.test.js
@@ -1,20 +1,30 @@
 const app = require("../../index");
 const db = require("../../models/index");
 const request = require("supertest");
+const Snack = db.snack;
 
-describe('Purchase router', () => {
-  describe('POST /api/customer/items/:itemId/purchases', () => {
+describe("Purchase router", () => {
+  describe("POST /api/customer/items/:itemId/purchases", () => {
     it("has successful status code", () => {
-      return request(app)
-      .post("/api/customer/items/:itemId/purchases")
-      .expect(200);
+      return Snack.create({
+        description: "Reese's",
+        price: 75,
+        quantity: 10
+      }).then(snack => {
+        return request(app)
+          .post("/api/customer/items/1/purchases")
+          .then(res => {
+            expect(200);
+          })
+      });
     });
-    //
-    // it('allows a customer to purchase an item if they have enough money and quantity > 1', () => {
+
+    // fit('allows a customer to purchase an item if they have enough money and quantity >= 1', () => {
     //   return request(app)
     //   .post("/api/customer/items/:itemId/purchases")
-    //   .expect()
+    //   .then(res => {
+    //     expect(res.body.data[0].quantity).toBeTruthy();
+    //   }
     // });
-
   });
 });
